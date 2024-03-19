5e5dc385e725b812000a5cccbb142007d5c5d7c7
Tushar Singh <viptusharsingh2000@gmail.com>
2024-03-19 19:53:16 +0530

    static changes

--- Ecommerce_website/settings.py
+++ Ecommerce_website/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'Ecommerce_app/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Ecommerce_app', 'static'),
